4e486d685a992fe43c9c85204e72c54a261d1d95
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-12 11:00:51 -0600

    add buttons => todo list item component

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -2,6 +2,8 @@
 const TodoListItem = ({ todo }) => (
   <div className="todo-item-conatiner">
     <h3>{todo?.text}</h3>
+    <button>Mark As Completed</button>
+    <button>Remove</button>
   </div>
 )
 
