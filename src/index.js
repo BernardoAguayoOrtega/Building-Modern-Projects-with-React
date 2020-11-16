b0f8aba41ee977b73f7549bce67667c6e4481af1
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-16 11:58:25 -0600

    Add persist data into redux config

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import { configureStore } from './store';
 import App from './App.js';
 
 const root = document.getElementById('root');
 
+const store = configureStore();
+
+const persistor = persistStore(store);
+
 render(
-	<Provider store={configureStore()}>
-		<App />
+	<Provider store={store}>
+		<PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+			<App />
+		</PersistGate>
 	</Provider>,
 	root,
 );
