b6b1c1335409dd0416fac75c7a70aa2b68c71d09
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-18 10:47:02 -0600

    add async operations with thunks

--- src/todos/NewTodoForm.js
+++ src/todos/NewTodoForm.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 // connect component
 import { connect } from 'react-redux';
 // import action
-import { createTodo } from './actions';
+import { addTodoRequest } from './thunks';
 // import styles
 import './NewTodoForm.css';
 
@@ -44,7 +44,7 @@ const mapStateToProps = (state) => ({
 });
 
 const mapDispatchToProps = (dispatch) => ({
-	onCreatePressed: (text) => dispatch(createTodo(text)),
+	onCreatePressed: (text) => dispatch(addTodoRequest(text)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -2,6 +2,7 @@ import {
 	loadTodosInProgress,
 	loadTodosSuccess,
 	loadTodosFailure,
+	createTodo,
 } from './actions';
 
 export const displayAlert = () => () => window.alert('hello');
@@ -15,6 +16,26 @@ export const loadTodos = () => async (dispatch, getState) => {
 		const todos = await response.json();
 
 		dispatch(loadTodosSuccess(todos));
+	} catch (error) {
+		dispatch(loadTodosFailure());
+	}
+};
+
+export const addTodoRequest = (text) => async (dispatch) => {
+	try {
+		const body = JSON.stringify({ text });
+
+		const response = await fetch('http://localhost:8080/todos', {
+			headers: {
+				'Content-type': 'application/json',
+			},
+			method: 'post',
+			body,
+		});
+
+		const todo = await response.json();
+
+		dispatch(createTodo(todo));
 	} catch (error) {
 		dispatch(loadTodosFailure());
 	}
