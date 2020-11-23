f7af753294be981e1f62628047a4d06807a23f9c
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-23 10:34:01 -0600

    Change todo list item styles for styled components

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -1,24 +1,60 @@
-// import styles
-import './TodoListItem.css';
+import styled from 'styled-components';
+
+const TodoItemContainer = styled.div`
+	background: #fff;
+	border-radius: 8px;
+	margin-top: 8px;
+	padding: 16px;
+	position: relative;
+	box-shadow: 0 4px 8px grey;
+`;
+
+const ButtonContainer = styled.div`
+	position: absolute;
+	right: 12px;
+	bottom: 12px;
+`;
+
+const CompletedButton = styled.button`
+	font-size: 16px;
+	padding: 8px;
+	border: none;
+	border-radius: 8px;
+	outline: none;
+	cursor: pointer;
+	display: inline-block;
+	background-color: lightblue;
+`;
+const RemoveButton = styled.button`
+	font-size: 16px;
+	padding: 8px;
+	border: none;
+	border-radius: 8px;
+	outline: none;
+	cursor: pointer;
+	display: inline-block;
+	background-color: lightcoral;
+	margin-left: 8px;
+`;
 
 // todo list item component
 const TodoListItem = ({ todo, onRemovedPress, isCompleted }) => (
-	<div className={`todo-item-container ${todo.isCompleted && 'disable'}`}>
+	<TodoItemContainer isCompleted={isCompleted}>
 		<h3>{todo?.text}</h3>
-		<div className='button-container'>
-			<button
+		<ButtonContainer>
+			<CompletedButton
 				disabled={todo.isCompleted}
 				className='completed-button'
 				onClick={() => isCompleted(todo?.id)}>
 				Mark As Completed
-			</button>
-			<button
+			</CompletedButton>
+			<RemoveButton
 				className='remove-button'
 				onClick={() => onRemovedPress(todo?.id)}>
 				Remove
-			</button>
-		</div>
-	</div>
+			</RemoveButton>
+		</ButtonContainer>
+	</TodoItemContainer>
 );
 
 export default TodoListItem;
