88187e3e65ec3c7d2722695598dea2a202be3507
Bernardo Aguayo Ortega <bernardo.aguayo.ortega@gmail.com>
2020-11-23 10:55:32 -0600

    Chnage new todo form styles for styled component

--- src/todos/NewTodoForm.js
+++ src/todos/NewTodoForm.js
@@ -5,8 +5,36 @@ import { connect } from 'react-redux';
 // import action
 import { addTodoRequest } from './thunks';
 import { getTodos } from './selectors'
-// import styles
-import './NewTodoForm.css';
+import styled from 'styled-components'
+
+const FormContainer = styled.div`
+  border-radius: 8px;
+  padding: 16px;
+  text-align: center;
+  box-shadow: 0 4px 8px grey;
+`
+
+const NewTodoInput = styled.input`
+  font-size: 16px;
+  padding: 8px;
+  border: none;
+  border-bottom: 2px solid #ddd;
+  border-radius: 8px;
+  width: 70%;
+  outline: none;
+`
+
+const NewTodoButton = styled.button`
+  font-size: 16px;
+  padding: 8px;
+  border: none;
+  border-radius: 8px;
+  outline: none;
+  cursor: pointer;
+  margin-left: 8px;
+  width: 20%;
+  background-color: lightblue;
+`
 
 // new todo form component
 const NewTodoForm = ({ todos, onCreatePressed }) => {
@@ -14,16 +42,14 @@ const NewTodoForm = ({ todos, onCreatePressed }) => {
 	const [inputValue, setInputValue] = useState(undefined);
 
 	return (
-		<div className='new-todo-form'>
-			<input
+		<FormContainer>
+			<NewTodoInput
 				placeholder='type a new todo'
 				type='text'
-				className='new-todo-input'
 				value={inputValue}
 				onChange={(e) => setInputValue(e.target.value)}
 			/>
-			<button
-				className='new-todo-button'
+			<NewTodoButton
 				onClick={() => {
 					const isDuplicateText = todos.some(
 						(todo) => todo.text === inputValue,
@@ -35,8 +61,8 @@ const NewTodoForm = ({ todos, onCreatePressed }) => {
 					}
 				}}>
 				Add
-			</button>
-		</div>
+			</NewTodoButton>
+		</FormContainer>
 	);
 };
 
